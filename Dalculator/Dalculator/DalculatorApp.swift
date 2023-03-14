3a7102d2477f57435fb194bd55ce72a66fae5485
cys <cys@iquest.kr>
2023-03-14 10:35:28 +0900

    init: 프로젝트 파일 추가

---
+++ Dalculator/Dalculator/DalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DalculatorApp.swift
+//  Dalculator
+//
+//  Created by YuSeongChoi on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct DalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
